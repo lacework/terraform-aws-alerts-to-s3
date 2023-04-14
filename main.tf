3c12b94099ff3e65dae8e3c375b2c1a01e4ed6c5
djmctavish <87976925+djmctavish@users.noreply.github.com>
2023-04-14 11:13:21 +0100

    fix: s3 bucket ownership controls (#5)

--- main.tf
+++ main.tf
@@ -152,6 +152,14 @@ resource "aws_s3_bucket" "lacework_alerts_bucket" {
   bucket = var.aws_s3_bucket_name
 }
 
+resource "aws_s3_bucket_ownership_controls" "alerts_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.lacework_alerts_bucket.id
+
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.lacework_alerts_bucket.id
   acl    = "private"
